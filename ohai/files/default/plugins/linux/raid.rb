c1d44d2c6ebc834fd84030084e1d8cb4ae5690b9
Tim Smith <tsmith@llnw.com>
2014-05-02 14:23:04 -0700

    Rubocop fixes and detect LSI MPT controllers
    There are LSI Fusion MPT controllers that weren’t picked up by the old
    string.

--- ohai/files/default/plugins/linux/raid.rb
+++ ohai/files/default/plugins/linux/raid.rb
@@ -6,25 +6,25 @@ raid Mash.new
 # 05:00.0 "RAID bus controller" "Hewlett-Packard Company" "Smart Array G6 controllers" -r01 "Hewlett-Packard Company" "Smart Array P410i"
 # 11:00.0 "RAID bus controller" "Areca Technology Corp." "Device 1880" -r05 "Areca Technology Corp." "Device 1880"
 # 01:00.0 "RAID bus controller" "LSI Logic / Symbios Logic" "MegaRAID SAS 1078" -r04 "Dell" "PERC 6/i Integrated RAID Controller"
-
+# 05:00.0 "Serial Attached SCSI controller" "LSI Logic / Symbios Logic" "SAS2008 PCI-Express Fusion-MPT SAS-2 [Falcon]" -r02 "LSI Logic / Symbios Logic" "SAS2008 PCI-Express Fusion-MPT SAS-2 [Falcon]"
 
 # PCI ID / TYPE / VENDOR / DEVICE NAME / -rREVISION / SUBVENDOR / SUBSYSTEM NAME
-re=Regexp.new("(.*) \"(.*)\" \"(.*)\" \"(.*)\" -r(.*) \"(.*)\" \"(.*)\"")
+regex = Regexp.new("(.*) \"(.*)\" \"(.*)\" \"(.*)\" -r(.*) \"(.*)\" \"(.*)\"")
 
-devices=Array.new
+devices = []
 
-`/usr/bin/lspci -m | /bin/grep -i raid`.each_line do |l|
-  m=re.match(l)
-  if m then
-    data=Hash.new
+`/usr/bin/lspci -m | /bin/grep -iE 'raid|MPT'`.each_line do |l|
+  m = regex.match(l)
+  if m
+    data = Hash.new
     data[:pciid] = m[1]
     data[:vendor] = m[3]
     data[:susbsystem] = m[7]
-    data[:devicename]=m[4]
-    data[:revision]=m[5]
-    data[:subvendor]=m[6]
+    data[:devicename] = m[4]
+    data[:revision] = m[5]
+    data[:subvendor] = m[6]
     devices.push(data)
   end
 end
 
-raid[:devices]=devices
+raid[:devices] = devices
