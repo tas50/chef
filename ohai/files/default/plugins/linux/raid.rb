e7381fe299d79d49613631c1ef1a7e51d23b155a
Tim Smith <tsmith@llnw.com>
2014-06-03 12:31:41 -0700

    Update RAID plugin for Ohai 7
    Also fix run failures on CentOS by using relative path to to lspci, and
    switch to shell_out vs. back ticks

--- ohai/files/default/plugins/linux/raid.rb
+++ ohai/files/default/plugins/linux/raid.rb
@@ -1,30 +1,44 @@
-provides 'raid/devices'
+#
+# Author:: Nicolas Szalay <nico@rottenbytes.info>
+# Plugin:: raid
+#
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 
-raid Mash.new
+Ohai.plugin(:Raid) do
 
-# Sample data
-# 05:00.0 "RAID bus controller" "Hewlett-Packard Company" "Smart Array G6 controllers" -r01 "Hewlett-Packard Company" "Smart Array P410i"
-# 11:00.0 "RAID bus controller" "Areca Technology Corp." "Device 1880" -r05 "Areca Technology Corp." "Device 1880"
-# 01:00.0 "RAID bus controller" "LSI Logic / Symbios Logic" "MegaRAID SAS 1078" -r04 "Dell" "PERC 6/i Integrated RAID Controller"
-# 05:00.0 "Serial Attached SCSI controller" "LSI Logic / Symbios Logic" "SAS2008 PCI-Express Fusion-MPT SAS-2 [Falcon]" -r02 "LSI Logic / Symbios Logic" "SAS2008 PCI-Express Fusion-MPT SAS-2 [Falcon]"
+  provides 'raid/devices'
 
-# PCI ID / TYPE / VENDOR / DEVICE NAME / -rREVISION / SUBVENDOR / SUBSYSTEM NAME
-regex = Regexp.new("(.*) \"(.*)\" \"(.*)\" \"(.*)\" -r(.*) \"(.*)\" \"(.*)\"")
+  collect_data(:linux) do
+    raid Mash.new
 
-devices = []
+    # Sample data
+    # 05:00.0 "RAID bus controller" "Hewlett-Packard Company" "Smart Array G6 controllers" -r01 "Hewlett-Packard Company" "Smart Array P410i"
+    # 11:00.0 "RAID bus controller" "Areca Technology Corp." "Device 1880" -r05 "Areca Technology Corp." "Device 1880"
+    # 01:00.0 "RAID bus controller" "LSI Logic / Symbios Logic" "MegaRAID SAS 1078" -r04 "Dell" "PERC 6/i Integrated RAID Controller"
+    # 05:00.0 "Serial Attached SCSI controller" "LSI Logic / Symbios Logic" "SAS2008 PCI-Express Fusion-MPT SAS-2 [Falcon]" -r02 "LSI Logic / Symbios Logic" "SAS2008 PCI-Express Fusion-MPT SAS-2 [Falcon]"
 
-`/usr/bin/lspci -m | /bin/grep -iE 'raid|MPT'`.each_line do |l|
-  m = regex.match(l)
-  if m
-    data = Hash.new
-    data[:pciid] = m[1]
-    data[:vendor] = m[3]
-    data[:susbsystem] = m[7]
-    data[:devicename] = m[4]
-    data[:revision] = m[5]
-    data[:subvendor] = m[6]
-    devices.push(data)
+    # PCI ID / TYPE / VENDOR / DEVICE NAME / -rREVISION / SUBVENDOR / SUBSYSTEM NAME
+    regex = Regexp.new("(.*) \"(.*)\" \"(.*)\" \"(.*)\" -r(.*) \"(.*)\" \"(.*)\"")
+
+    devices = []
+
+    so = shell_out("lspci -m | /bin/grep -iE 'raid|MPT'")
+    so.stdout.each_line do |l|
+      m = regex.match(l)
+      if m
+        data = Hash.new
+        data[:pciid] = m[1]
+        data[:vendor] = m[3]
+        data[:susbsystem] = m[7]
+        data[:devicename] = m[4]
+        data[:revision] = m[5]
+        data[:subvendor] = m[6]
+        devices.push(data)
+      end
+    end
+
+    raid[:devices] = devices
   end
 end
-
-raid[:devices] = devices
